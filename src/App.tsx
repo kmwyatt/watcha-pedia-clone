cb1e8b6ebc951bb7da01a70bffe8b54d4e87e951
kmwyatt <arc2437@naver.com>
2022-01-27 12:30:40 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import MovieDetail from "./pages/MovieDetail";
+import TvDetail from "./pages/TvDetail";
+import TvPage from "./pages/TvPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/tv" component={TvPage} />
+        <Route path="/movie/:id" component={MovieDetail} />
+        <Route path="/tv/:id" component={TvDetail} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/TvDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TvDetailPage: React.FC = () => {
+  return <div>TV Detail Page</div>;
+};
+
+export default TvDetailPage;

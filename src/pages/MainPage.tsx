2021838e202da4c22c79ef5f186322de5e85fb0b
kmwyatt <arc2437@naver.com>
2022-01-27 13:11:27 +0900

    Add footer

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const MainPage: React.FC = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
